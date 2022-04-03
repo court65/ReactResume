9ad7fb69fd0ea71271c7683b93aa348df0af0610
court65 <court_re@yahoo.com>
2022-04-03 13:30:02 -0500

    Added Nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className="nav bg-primary">
+        <h2 className='name '>Court Evans</h2>
+        <ul className='navul'>
+            <li>about</li>
+            <li>skills</li>
+            <li>project</li>
+            <li>contact</li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav
